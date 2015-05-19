b74b600e0da2f2af2ad8df676f70341eba72812d
Kashif Shaikh <kshaikh@angus-systems.com>
2015-05-19 12:50:03 -0400

    Force load FLKAutoLayout

--- Asgl.iOSLibBindings/ORStackView/ORStackView.linkwith.cs
+++ Asgl.iOSLibBindings/ORStackView/ORStackView.linkwith.cs
@@ -2,3 +2,4 @@ using System;
 using ObjCRuntime;
 
 [assembly: LinkWith ("ORStackView.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith("FLKAutoLayout.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64, SmartLink = false, ForceLoad = true)]
